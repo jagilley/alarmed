2ce2080ea05e3287a7324277ce7e60bdf19547c9
jagilley <37783831+jagilley@users.noreply.github.com>
2019-01-09 14:33:29 -0600

    caf

--- alarm.py
+++ alarm.py
@@ -38,7 +38,7 @@ else:
 	command = "espeak"
 	with open("/home/pi/Code/alarmed/greetings.txt", 'r') as f:
 		greetings = f.read().split("\n")
-	subprocess.call(["omxplayer", "--vol", "-600", "/home/pi/Code/alarmed/audio/Collins Avenue Short.mp3"])
+	subprocess.call(["omxplayer", "--vol", "-600", "/home/pi/Code/alarmed/audio/Collins Avenue Short.caf"])
 
 finalText = random.choice(greetings) + " " + weather.toSpeak + " " + cal.toSpeak
 print(finalText)
