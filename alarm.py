46611f246b131b529d53f2e77fa5c799cb97f2ab
jagilley <37783831+jagilley@users.noreply.github.com>
2019-01-09 13:24:26 -0600

    added cli args

--- alarm.py
+++ alarm.py
@@ -5,14 +5,21 @@ import random
 import subprocess
 import random
 import platform
+import argparse
 
-import getAlarm
-wakeupTime = getAlarm.wakeupTime
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action='store_true', help='immediately trigger alarm')
+args = parser.parse_args()
 
-theFinalCountdown = (wakeupTime - datetime.datetime.now()).seconds
-print("Sleeping for", theFinalCountdown, "seconds")
+if not args.debug:
+	import getAlarm
+	wakeupTime = getAlarm.wakeupTime
 
-#time.sleep(theFinalCountdown)
+	theFinalCountdown = (wakeupTime - datetime.datetime.now()).seconds
+	print("Sleeping for", theFinalCountdown, "seconds")
+	time.sleep(theFinalCountdown)
+else:
+	print("Entering debug mode, triggering alarm immediately")
 
 # Time for the alarm to go off
 print("Wake Up!")
